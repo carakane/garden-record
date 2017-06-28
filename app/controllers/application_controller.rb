d69bdd6b7e97cde49095c2fb9436d6eeabfb73ac
jessica fletcher <cara.kane@gmail.com>
2017-06-28 17:53:09 +0000

    setup basics

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,30 @@
+require './config/environment'
+require "rack-flash"
+
+class ApplicationController < Sinatra::Base
+  register Sinatra::ActiveRecordExtension
+  set :session_secret, "my_application_secret"
+  set :views, Proc.new { File.join(root, "../views/") }
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
+    use Rack::Flash
+  end
+
+  helpers do
+		def logged_in?
+			!!session[:user_id]
+		end
+
+		def current_user
+			User.find(session[:user_id])
+		end
+	end
+
+  get '/' do
+    erb :index
+  end
+end
