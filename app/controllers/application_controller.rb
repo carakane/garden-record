9da452231dc84e82a021d4da0fb216c30da0a6d9
jessica fletcher <cara.kane@gmail.com>
2017-06-28 18:13:40 +0000

    added basic controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ require "rack-flash"
 
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
-  set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
 
   configure do
