a6b07eede839327f62b8fd78e8a46239c730ef64
jessica fletcher <cara.kane@gmail.com>
2017-06-28 18:20:14 +0000

    added new location route

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,14 @@
 class LocationsController < ApplicationController
+
+  get '/locations/new' do
+    if logged_in?
+      erb :'/locations/new_plant'
+    else redirect '/login'
+    end
+  end
+
+  post '/locations/new' do
+    # binding.pry
+    @location = Location.create(params["location"])
+  end
 end
