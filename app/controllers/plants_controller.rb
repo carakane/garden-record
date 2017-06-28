3499dc0f46f04c49217f2196b45223acdbd16080
jessica fletcher <cara.kane@gmail.com>
2017-06-28 18:18:03 +0000

    added new plant route

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,2 +1,14 @@
 class PlantsController < ApplicationController
+
+  get '/plants/new' do
+    if logged_in?
+      erb :'/plants/new_plant'
+    else redirect '/login'
+    end
+  end
+
+  post '/plants/new' do
+    # binding.pry
+    @plant = Plant.create(params["plant"])
+  end
 end
