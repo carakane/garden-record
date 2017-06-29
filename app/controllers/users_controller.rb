72e5679ac8261b92476ecbd484bd675d12dc49c0
jessica fletcher <cara.kane@gmail.com>
2017-06-29 14:15:42 +0000

    added login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,17 +2,26 @@ class UsersController < ApplicationController
 
   get '/signup' do
     if logged_in?
-      redirect "/garden"
+      redirect "/users/:username"
     else
       erb :'/users/new_user'
     end
   end
 
   post '/signup' do
-    # binding.pry
     @user = User.create(params["user"])
   end
 
+  get '/login' do
+    erb :'/users/login'
+  end
 
+  post '/login' do
+    @user = User.find_by(:username => params["username"])
+    if @user.password == params["password"]
+      session[:user_id] = @user.id
+      redirect '/users/:username'
+    end
+  end
 
 end
