a97c4481fad26768436d7517dda62674d7635569
jessica fletcher <cara.kane@gmail.com>
2017-07-02 13:54:34 +0000

    added new locations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,13 +2,23 @@ class LocationsController < ApplicationController
 
   get '/locations/new' do
     if logged_in?
-      erb :'/locations/new_plant'
+      erb :'/locations/new_location'
     else redirect '/login'
     end
   end
 
   post '/locations/new' do
     # binding.pry
+    @user = current_user
     @location = Location.create(params["location"])
+    @user.locations << @location
+    redirect "/locations/#{@location.id}"
   end
+
+  get '/locations/:id' do
+    @user = current_user
+    @location = Location.find_by(:id => params[:id])
+    erb :'/locations/show'
+  end
+
 end
