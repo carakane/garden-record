9bdbfa9a04da999e25839ad622f273ff0b11a425
jessica fletcher <cara.kane@gmail.com>
2017-07-02 20:06:37 +0000

    added login failure redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,7 @@ class UsersController < ApplicationController
     if @user && @user.authenticate(params["password"])
       session[:user_id] = @user.id
       redirect "/users/#{@user.username}"
+    else redirect '/users/login'
     end
   end
 
